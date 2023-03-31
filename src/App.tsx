c71820bd01d95f8597446c6121fac29def86a7bd
Kristen Lavavej <lavavej@gmail.com>
2023-03-31 08:03:53 -0700

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+	  <Auth0Provider
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+	      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+	      authorizationParams={{
+	        redirect_uri: window.location.origin,
+		      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+	        scope: "openid profile admin",
+        }}
+	  >
+	    <App />
+	  </Auth0Provider>
+  </React.StrictMode>
+);
