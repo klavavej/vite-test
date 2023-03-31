e10f90219b2bf5fb3abc48b007ed01c4c1b62e1b
Kristen Lavavej <lavavej@gmail.com>
2023-03-31 10:08:45 -0700

    unsmart

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import “./index.css”;
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
