a76d038ca075c46134002ba8d337ddb01fc1e8b7
Kristen Lavavej <lavavej@gmail.com>
2023-03-31 07:34:59 -0700

    typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from "@auth/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
