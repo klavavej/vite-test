54a1fd490a97b590b4d623f0a88a38f43116e402
Kristen Lavavej <lavavej@gmail.com>
2023-03-31 08:27:17 -0700

    add function

---
+++ netlify/functions/test.ts
@@ -0,0 +1,23 @@
+import { Handler } from "@netlify/functions";
+import { wrap } from "@netlify/integrations";
+import { withPlanetscale } from "@netlify/planetscale";
+import { withAuth0 } from "@netlify/auth0";
+
+const withIntegrations = wrap(withAuth0, withPlanetscale)
+
+export const handler: Handler = withIntegrations(
+  async (event, context) => {
+    const { connection } = context.planetscale;
+
+    const {rows: users } = await connection.execute("SELECT * FROM  users");
+
+    return	{
+      statusCode: 200,
+      body: JSON.stringify(users),
+    };
+  },
+  {
+    required: true,
+    roles: ["admin"],
+  }
+)
