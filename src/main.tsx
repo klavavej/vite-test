5a7354442727bc66048c9b97df680d6472176401
Kristen Lavavej <lavavej@gmail.com>
2023-03-31 07:22:41 -0700

    add fe

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Auth0Provider } from "@auth/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	  <Auth0Provider
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+	      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+	      authorizationParams={{
+	        redirect_uri: window.location.origin,
+		      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+	        scope: "openid profile admin",
+        }}
+	  >
+	    <App />
+	  </Auth0Provider>
+  </React.StrictMode>
+);
